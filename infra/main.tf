d4bb896b12e8f3283db3004562d113bbe9f97dae
Rohan Rajesh <your-email@example.com>
2026-06-12 15:41:12 +0530

    feat: Terraform VPC module with subnets, IGW, NAT, route tables

---
+++ infra/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+module "vpc" {
+  source               = "./modules/vpc"
+  project_name         = var.project_name
+  vpc_cidr             = var.vpc_cidr
+  public_subnet_cidrs  = var.public_subnet_cidrs
+  private_subnet_cidrs = var.private_subnet_cidrs
+}
---
+++ infra/variables.tf
@@ -0,0 +1,19 @@
+variable "aws_region" {
+  default = "ap-south-1"
+}
+
+variable "project_name" {
+  default = "rohan-taskapi"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  default = ["10.0.3.0/24", "10.0.4.0/24"]
+}
